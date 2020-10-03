fcb018d064ad7d76e96ee1ab267cf432784d95ea
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-03 04:34:55 -0700

    SAVE

--- routes/v0/count.route.js
+++ routes/v0/count.route.js
@@ -6,14 +6,16 @@ const func = require('od-utility');
 
 router.get('/zipcode', async (req, res, next) => {
     try {
-        res.json({
-            status: true, message: 'record found', payload: {
+        res.json(
+            func.configSuccess({
+
                 query: '/count/zipcode?category=mexican&zipcode=91709',
                 category: 'mexican',
                 zipcode: 91709,
                 count: 12
-            }
-        });
+
+            })
+        );
     } catch (e) {
         next(e);
     }
@@ -22,14 +24,13 @@ router.get('/zipcode', async (req, res, next) => {
 
 router.get('/area', async (req, res, next) => {
     try {
-        res.json({
-            status: true, message: 'record found', payload: {
+        res.json(func.configSuccess({
                 query: 'category=chinese&lat=33.334423&lng=-117.223234&radius=21',
                 category: 'mexican',
-                radius:21,
+                radius: 21,
                 count: 23
-            }
-        });
+            })
+        );
     } catch (e) {
         next(e);
     }
