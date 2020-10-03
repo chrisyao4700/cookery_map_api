124c5dfe61b24e9bfc831a33edbd5bf75a99a667
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-03 04:32:13 -0700

    ADD AREA DEMO

--- routes/v0/count.route.js
+++ routes/v0/count.route.js
@@ -20,4 +20,19 @@ router.get('/zipcode', async (req, res, next) => {
 });
 
 
+router.get('/area', async (req, res, next) => {
+    try {
+        res.json({
+            status: true, message: 'record found', payload: {
+                query: 'category=chinese&lat=33.334423&lng=-117.223234&radius=21',
+                category: 'mexican',
+                radius:21,
+                count: 23
+            }
+        });
+    } catch (e) {
+        next(e);
+    }
+});
+
 module.exports = router;
