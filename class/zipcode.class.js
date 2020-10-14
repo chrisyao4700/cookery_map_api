4fbadae884b7a9b3d5385c11152681fc8beddd6a
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-14 14:05:04 -0700

    ADD SCORE COMPONENT IN ITEM

--- class/zipcode.class.js
+++ class/zipcode.class.js
@@ -8,7 +8,7 @@ class CMZipcode extends ODInstance {
 
         condition
             .configComplexConditionKeys('cm_address', ['addr_str'])
-            .configComplexConditionKeys('cm_location', ['name', 'identifier'])
+            .configComplexConditionKeys('cm_location', ['name', 'identifier','score'])
             .configComplexConditionJoin('cm_location', 'address_id', 'cm_address', 'LEFT JOIN')
             .configComplexConditionQueryItem('cm_address', 'zip', zipcode)
             .configComplexConditionQueryItem('cm_address', 'status', 1)
